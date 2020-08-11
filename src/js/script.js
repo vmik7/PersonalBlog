c388d2c2375b3a1f78086d51a9cd9ca849e91a75
Ivan Mikerin <vmik7@yandex.ru>
2020-08-11 16:47:16 +0300

    Added header switch

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,77 @@ testWebP(function (support) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+
+
+// Header switcher ************************************************
+const headerWrapper = document.querySelector('.header__wrapper');
+const headerTrack = document.querySelector('.header__banners-track');
+const banners = document.querySelectorAll('.banner');
+const headerDotsWrapper = document.querySelector('.banner-switch__dots');
+const headerPrevButton = document.querySelector('.banner-switch__right-arrow');
+const headerNextButton = document.querySelector('.banner-switch__left-arrow');
+
+let headerDotClassName = 'dot';
+let bannersCount = banners.length;
+let activeBanner = 1;
+let bannerHeight = banners[0].offsetHeight;
+let regExprPx = /[0-9-.]+(?=px)/g
+
+
+headerDotsWrapper.innerHTML = '';
+for (let i = 0; i < bannersCount; i++) {
+    headerDotsWrapper.innerHTML += `<div class="${headerDotClassName}"></div>`;
+}
+
+const headerDots = document.querySelectorAll(`.banner-switch__dots .${headerDotClassName}`);
+
+headerTrack.style.transform = `translate3d(0px,-${bannerHeight * activeBanner}px,0px)`;
+headerDots[bannersCount - 1 - activeBanner].classList.add(`${headerDotClassName}_active`);
+
+let setBanner = function(bannerIndex = activeBanner) {
+    activeBanner = bannerIndex;
+
+    // console.log(headerTrack.style.transform.match(regExprPx)[0]);
+
+    headerTrack.style.transition = 'all 0.3s ease';
+    headerTrack.style.transform = `translate3d(0px,-${bannerHeight * activeBanner}px,0px)`;
+
+    for (let i = 0; i < bannersCount; i++) {
+        if (i === activeBanner) {
+            headerDots[bannersCount - 1 - i].classList.add(`${headerDotClassName}_active`);
+        }
+        else {
+            headerDots[bannersCount - 1 - i].classList.remove(`${headerDotClassName}_active`);
+        }
+    }
+
+    // console.log(headerTrack.style.transform.match(regExprPx)[0]);
+};
+
+for (let dot of headerDots) {
+    dot.addEventListener('click', function() {
+        let bannerIndex = activeBanner;
+        for (let i = 0; i < bannersCount; i++) {
+            if (headerDots[bannersCount - 1 - i] == this) {
+                bannerIndex = i;
+                break;
+            }
+        }
+        setBanner(bannerIndex);
+    });
+}
+
+headerPrevButton.addEventListener('click', function() {
+    activeBanner--;
+    if (activeBanner < 0) {
+        activeBanner += bannersCount;
+    }
+    setBanner();
+});
+headerNextButton.addEventListener('click', function() {
+    activeBanner++;
+    if (activeBanner >= bannersCount) {
+        activeBanner -= bannersCount;
+    }
+    setBanner();
+});
